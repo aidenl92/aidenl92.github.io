9c8eafc374dd8a14d656ad395021135d76ed9c25
aidenl92 <89806557+aidenl92@users.noreply.github.com>
2021-11-30 13:59:59 -0500

    Update linster-activity13.js

--- itis3135/activity13/linster-activity13.js
+++ itis3135/activity13/linster-activity13.js
@@ -9,15 +9,18 @@ $(document).ready(function() {
         error: function(xhr, status, error) {
             alert("Error: " + xhr.status + " - " + error);
         },
-        dataType: "xml",
+        dataType: "json",
         success: function(data) {
-            $("#team").html("");
-            $(data).find("management").children().each(function() {
-                var xmlDoc = $(this);
-                $("#team").append
-                ("<h3>" + xmlDoc.find("name").text() + "</h3>" +
-                          xmlDoc.find("title").text() + "<br>" +
-                          xmlDoc.find("bio").text() + "<br>");
+            $.getJSON("team.json" , function(data){
+               $.each(data,function(){
+                   $.each(this, function(key, value){
+                       $('#team').append(
+                         "Name: "+ value.name + "<br>"+
+                         "Title: "+ value.title + "<br>" +
+                         "Bio: " + value.bio + "<br>"
+                       );
+                   });
+               }) ;
             });
         }
     });
