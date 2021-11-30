6f6c276d4c0b21c45d0a841e2dad5154bb4a9153
aidenl92 <89806557+aidenl92@users.noreply.github.com>
2021-11-30 18:43:27 -0500

    Update linster-activity14.js

--- itis3135/activity14/linster-activity14.js
+++ itis3135/activity14/linster-activity14.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-	
+
+	$('#toobin').click(function(){
+        $.ajax({
+            type: "get",
+            url"json_files/toobin.json",
+            success: function(data){
+				$.getJSON('toobin.json', function(data){
+					$.each(data,function(){
+						$.each(this, function(key, val){
+							$("main").html(
+								
+							);
+						});
+					});
+				});
+        }); //end toobin ajax
+    }); //end toobin click
+
+    $('#sorkin').click(function(){
+
+    }); //end sorkin click
+
+    $('#chua').click(function(){
+
+    }); //end chua click
+
+    $('#sampson').click(function(){
+
+    }); //end sampson click
 }); // end ready
