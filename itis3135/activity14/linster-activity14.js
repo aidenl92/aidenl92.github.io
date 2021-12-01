acc1f96b4a0dd6ecd3c5d02b9d172e1fb78bbbdf
aidenl92 <89806557+aidenl92@users.noreply.github.com>
2021-11-30 21:15:54 -0500

    Update linster-activity14.js

--- itis3135/activity14/linster-activity14.js
+++ itis3135/activity14/linster-activity14.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 			url: "json_files/"+ selectedSpeaker + ".json",
 			dataType: "json",
 			success: function(data){
-				$("#main").html('');
+				$("#main").empty();
 				$.getJSON("json_files/"+ selectedSpeaker + ".json", function(data){
 					$.each(data,function(){
 						$.each(this, function(key, val){
