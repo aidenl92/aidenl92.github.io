9f6049c47d05888f69d5922946129bb6787e7d8e
aidenl92 <89806557+aidenl92@users.noreply.github.com>
2021-11-30 19:24:31 -0500

    Update linster-activity14.js

--- itis3135/activity14/linster-activity14.js
+++ itis3135/activity14/linster-activity14.js
@@ -1,24 +1,43 @@
 $(document).ready(function() {
 
+	var selectedSpeaker = "";
+	var imgAlt = "";
+
 	$('#toobin').click(function(){
+		selectedSpeaker = "toobin";
+		imgAlt = "toobin_court";
         $.ajax({
             type: "get",
-            url"json_files/toobin.json",
+            url: "json_files/toobin.json",
             success: function(data){
+				$('main').html('');
 				$.getJSON('toobin.json', function(data){
 					$.each(data,function(){
 						$.each(this, function(key, val){
 							$("main").html(
-								
+								"<h1>" + val.title + "</h1>"+
+								"<h2>" + val.month + "</h2>"+
+								"<h3>" + val.name + "</h3>"+
+								"<img src='"+ val.image +"' alt='"+ imgAlt+"'>"+
+								"<p>"+ val.text + "</p>"
 							);
 						});
 					});
-				});
+				}); //end success function
         }); //end toobin ajax
     }); //end toobin click
 
     $('#sorkin').click(function(){
-
+		selectedSpeaker = "sorkin";
+		imgAlt = "sorkin_desk";
+		$.ajax({
+			type: "get",
+			url: "json_files/sorkin.json",
+			success: function(data){
+				getSpeakerInfo(data,selectedSpeaker,imgAlt);
+			});
+		});
+	
     }); //end sorkin click
 
     $('#chua').click(function(){
@@ -29,3 +48,18 @@ $(document).ready(function() {
 
     }); //end sampson click
 }); // end ready
+
+function getSpeakerInfo(data,speaker,imgAlt){
+	$("main").html("");
+	$.getJSON(speaker +".json"), function(data){
+		$.each(data,function(){
+			$.each(this,function(key,val){
+				"<h1>" + val.title + "</h1>"+
+				"<h2>" + val.month + "</h2>"+
+				"<h3>" + val.name + "</h3>"+
+				"<img src='"+ val.image +"' alt='"+ imgAlt+"'>"+
+				"<p>"+ val.text + "</p>"
+			});
+		});
+	});
+}
