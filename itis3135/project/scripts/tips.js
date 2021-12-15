57d4448cf10143f1a8eb10fad8c68abc4a107d92
aidenl92 <89806557+aidenl92@users.noreply.github.com>
2021-12-15 13:36:34 -0500

    project

--- itis3135/project/scripts/tips.js
+++ itis3135/project/scripts/tips.js
@@ -16,19 +16,7 @@ $(document).ready(function(){
 						);
 					});
 				});
-			});
-			//accordion widget
-			$("#accordion").accordion({
-					icons: {
-							  "header": "ui-icon-triangle-1-e",
-							  "activeHeader": "ui-icon-triangle-1-s"
-							},
-					collapsible: true
-			});
-					
-			$("#accordion").css({
-				"marginRight": "900px"
-			});
+			});	
 			
 		},
 		error: function(data){
