bb8876b9eba0e4d81886d6ce479bc8d90f858630
aidenl92 <89806557+aidenl92@users.noreply.github.com>
2021-12-15 13:28:16 -0500

    Update tips.js

--- itis3135/project/scripts/tips.js
+++ itis3135/project/scripts/tips.js
@@ -1,18 +1,5 @@
 $(document).ready(function(){
-	
-	//accordion widget
-			$("#accordion").accordion({
-					icons: {
-							  "header": "ui-icon-triangle-1-e",
-							  "activeHeader": "ui-icon-triangle-1-s"
-							},
-					collapsible: true
-			});
-					
-			$("#accordion").css({
-				"marginRight": "900px"
-			});
-			
+
 	//get json content through ajax
 	$.ajax({
 		type:"get",
@@ -29,6 +16,18 @@ $(document).ready(function(){
 						);
 					});
 				});
+			});
+			//accordion widget
+			$("#accordion").accordion({
+					icons: {
+							  "header": "ui-icon-triangle-1-e",
+							  "activeHeader": "ui-icon-triangle-1-s"
+							},
+					collapsible: true
+			});
+					
+			$("#accordion").css({
+				"marginRight": "900px"
 			});
 			
 		},
