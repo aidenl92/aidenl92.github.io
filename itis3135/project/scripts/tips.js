3060439c057e52fbe42240869d112d4c2246adfe
aidenl92 <89806557+aidenl92@users.noreply.github.com>
2021-12-15 13:01:58 -0500

    Update tips.js

--- itis3135/project/scripts/tips.js
+++ itis3135/project/scripts/tips.js
@@ -2,9 +2,7 @@ $(document).ready(function(){
 
 	//get json content through ajax
 	$.ajax({
-		type: "get",
-		url: "json_files/beginner-tips.json",
-		dataType:"json",
+		
 		success: function(data){
 			$("#accordion").empty();
 			$.getJSON("json_files/beginner-tips.json", function(data){
