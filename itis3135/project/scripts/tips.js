2074b02ac1859feb9a1b59e9d7add1a2f8adcc69
aidenl92 <89806557+aidenl92@users.noreply.github.com>
2021-12-15 01:19:59 -0500

    accordion

--- itis3135/project/scripts/tips.js
+++ itis3135/project/scripts/tips.js
@@ -18,6 +18,19 @@ $(document).ready(function(){
 	});
 	
 	//accordion widget
+	$("#accordion").accordion({
+		icons: {
+				  "header": "ui-icon-triangle-1-e",
+				  "activeHeader": "ui-icon-triangle-1-s"
+				},
+		collapsible: true
+	});
+	
+	$("#accordion").css({
+		"marginRight": "900px"
+	});
+	
+	
 	
 	console.log("js loaded");
 });
