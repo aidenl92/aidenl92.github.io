ec806aadbb6b7fe75d16ffccbd943017b2304d63
aidenl92 <89806557+aidenl92@users.noreply.github.com>
2021-12-16 01:40:21 -0500

    project

--- itis3135/project/scripts/pro-quiz.js
+++ itis3135/project/scripts/pro-quiz.js
@@ -49,7 +49,7 @@ $(document).ready(function(){
 					$("#correct-ans").show();
 					$("#ans-header").show();
 					for(var i=0; i < userAns.length; i++){
-						if(userAns[i] == ansList[i]){
+						if(JSON.stringify(userAns[i]) == JSON.stringify(ansList[i])){
 							score++;
 						}
 					}
